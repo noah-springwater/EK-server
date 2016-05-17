3e0ee7cc6a796cb3c2f61345b708d5e6233ccfca
Noah Springwater <nspringwater@gmail.com>
2016-05-17 19:29:42 -0400

    server

---
+++ main.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type':'text/plain'})
+  res.end('Whaddup server');
+}).listen(8081)
+
+console.log('server running at http://127.0.0.1:8081');
