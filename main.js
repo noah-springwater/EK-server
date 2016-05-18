43d60ae6313e41f024b9e8553edfd1ad7d2cfdb4
Noah Springwater <nspringwater@gmail.com>
2016-05-18 19:09:57 -0400

    logging current state of game

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-var http = require('http');
+var express =  require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var port = process.env.PORT || 3000;
+var socket = require('socket.io-client')('http://localhost:55555');
+var Canvas = require('canvas');
+var fs = require('fs');
+// socket.on('connect', function () {});
+socket.on('state', function (state) {
+  console.log(state);
+});
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type':'text/plain'})
-  res.end('Whaddup server');
-}).listen(8081)
+server.listen(port, function () {
+  console.log('Server listening on port', port);
+});
 
-console.log('server running at http://127.0.0.1:8081');
+app.get('/', function (req, res) {
+  res.send(state);
+});
